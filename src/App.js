d99a21908201d2ae06f336b0cbe69b597ecabb51
Vitalij Virsulo <vitalij.virsulo@notperfect.com>
2022-06-15 15:09:18 +0300

    4.0 Получение фильмов по API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
 
@@ -7,22 +8,26 @@ class App extends React.Component {
     movies: [] // movie list from API
   };
 
+  getMovies = async () => {
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+  }
+
   /**
    * When page is rendered call this method (default by ReactJs)
    */
-  componentDidMount() {
+  asynccomponentDidMount() {
+    this.getMovies();
     setTimeout( () => this.setState({isLoading: false}), 6000);
   }
-
   render() {
 
     const {isLoading} = this.state;
 
     return (
       <div>
-        { isLoading ? "loading.." : "Page loaded" }
+        { isLoading ? "Loading.." : "Page loaded" }
       </div>
-    )
+    );
 
   }
 
