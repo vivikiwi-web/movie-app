709f5e5ae028fe21a4803eea16a71aa7edf901e8
Vitalij Virsulo <vitalij.virsulo@notperfect.com>
2022-06-15 14:21:58 +0300

    setState function review

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-const App = () => {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+
+  state = {
+    count: 0,
+  };
+  
+  add = () => {
+    // this.setState({count: this.state.count - 1}); 
+    this.setState( current => (
+      { count: current.count + 1 }
+    ) );
+  }
+
+  minus = () => {
+    // this.setState({count: this.state.count - 1});
+    this.setState( current => (
+      { count: current.count - 1 }
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.add}>Plus</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
+
 }
 
 export default App;
